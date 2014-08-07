ec910808c5fc404c99b2aa660ead6e8651fb54c1
gnuhub <gnuhub@gmail.com>
2014-08-07 14:53:03 +0800

    add public key to authorized_keys

--- vagrant_root_ssh.sh
+++ vagrant_root_ssh.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -x
+vagrant ssh -c "sudo su -l root -c 'mkdir ~/.ssh/'"
 vagrant ssh -c "sudo su -l root -c 'echo \"root:gnuhub\" | chpasswd'"
-ssh-copy-id root@192.168.8.9
-ssh root@192.168.8.9
+vagrant ssh -c "sudo su -l root -c 'echo \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDoV2oZZSgHPsZtwQClLL5LbIp8oImvJ9wVxLSGNekGb/+cQ8wpRUZaPlmOAVbxRwONY8myUgYbbimffRu2aygtps8NoMhZKeqpzWcgnRA9I2Er/3PiB9T2XHx/plKuE6Z+eF//AwHE2hmkPCVI2sUfH7wye1KreHPVQHBrMPTnFx4exBTI2MEZHUPkGlJ8+oIH71fBXVJ7U3+5BqYIGtYIr8EcG3TQngf7G20e72mA6eWuvo/dyg6wRSCmbPhbwedxY7T2PyhZWsF8Qkt3E8j8rbZQY2NHdYyB/ma0RUjoh+ctKHBslzUMj08PIs8k3ymCAWlS8lauPRLkVZhUQRcZ gnuhub
+\" > ~/.ssh/authorized_keys'"
+ssh root@192.168.8.9 "echo 'connection is ok'"
