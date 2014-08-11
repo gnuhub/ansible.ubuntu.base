c3dc66d71d6b5a95c2d083edf16b00e14e05adc1
gnuhub <gnuhub@gmail.com>
2014-08-11 09:38:13 +0800

    change to build

--- vagrant_root_ssh.sh
+++ vagrant_root_ssh.sh
@@ -2,6 +2,6 @@
 set -x
 vagrant ssh -c "sudo su -l root -c 'mkdir ~/.ssh/'"
 vagrant ssh -c "sudo su -l root -c 'echo \"root:gnuhub\" | chpasswd'"
-vagrant ssh -c "sudo su -l root -c 'echo \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDoV2oZZSgHPsZtwQClLL5LbIp8oImvJ9wVxLSGNekGb/+cQ8wpRUZaPlmOAVbxRwONY8myUgYbbimffRu2aygtps8NoMhZKeqpzWcgnRA9I2Er/3PiB9T2XHx/plKuE6Z+eF//AwHE2hmkPCVI2sUfH7wye1KreHPVQHBrMPTnFx4exBTI2MEZHUPkGlJ8+oIH71fBXVJ7U3+5BqYIGtYIr8EcG3TQngf7G20e72mA6eWuvo/dyg6wRSCmbPhbwedxY7T2PyhZWsF8Qkt3E8j8rbZQY2NHdYyB/ma0RUjoh+ctKHBslzUMj08PIs8k3ymCAWlS8lauPRLkVZhUQRcZ gnuhub
+vagrant ssh -c "sudo su -l root -c 'echo \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCln97Y292n3E741ivfjsZCAa4dCcvizM5PQKppEY6fd5rhVFRGyuyIkvAvhoCNj6Ts9gGZUJSQ1rpYSfKkJJGVs5eZhBUA7cvNyCW7jT+Rds2AuxmYKP1aDHIKyX07eEIWTGuqj4E3tjGLBtaCpFAGK+oyxdeCVqsKiYZmtVy+u9HAoJ0BHHlIy6SAW9B2oC8zas84KySOVx0mzn9ftiYw1OQxAs4Mb8CD+pO2FBeLpLQTQy12IGav/ycfKv4NbTHQ4gUgSd7PEsmALT/FtY20awT8LTp3vZpQ6FfpdhPccJRhedMHLNQmcM6TMeLH5LbkW/xuhbqepFRkE/fOqxx7 home
 \" > ~/.ssh/authorized_keys'"
 ssh root@192.168.8.9 "echo 'connection is ok'"
